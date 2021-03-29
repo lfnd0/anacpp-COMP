bcc17be76ffb83802b6a89d50bfe55f93a4b5de4
luizfernando <luiz-fsilva37@hotmail.com>
2021-03-29 17:59:59 -0300

    Adição métodos classe GeradorAnaliseLexica

--- anacpp/src/br/edu/analisador/cpp/GeradorAnaliseLexicaCPP.java
+++ anacpp/src/br/edu/analisador/cpp/GeradorAnaliseLexicaCPP.java
@@ -1,8 +1,18 @@
 package br.edu.analisador.cpp;
 
+import java.io.File;
+import java.nio.file.Paths;
+
 public class GeradorAnaliseLexicaCPP {
 
 	public static void main(String[] args) {
-		
+		String rootPath = Paths.get("").toAbsolutePath().toString();
+		String subPath = "src/br/edu/analisador/cpp/";
+
+		String flexCPP = rootPath + subPath + "cpp.flex";
+
+		File file = new File(flexCPP);
+
+		jflex.Main.generate(file);
 	}
 }
