7109ae6e566b4515bf32cfe0fcd1c48ded02bd4d
luizfernando <luiz-fsilva37@hotmail.com>
2021-03-29 18:00:19 -0300

    Adição métodos classe AnalisadorLexicoCPP

--- anacpp/src/br/edu/analisador/cpp/AnalisadorLexicoCPP.java
+++ anacpp/src/br/edu/analisador/cpp/AnalisadorLexicoCPP.java
@@ -1,9 +1,21 @@
 package br.edu.analisador.cpp;
 
+import java.nio.file.Paths;
+
 public class AnalisadorLexicoCPP {
 
 	public static void main(String[] args) {
+		String rootPath = Paths.get("").toAbsolutePath().toString();
+		String subPath = "src/br/edu/analisador/cpp/";
 		
+		String inputCPP = rootPath + subPath + "input.cpp";
+		
+		TokensCPP tokensCPP;
+		
+//		LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(new FileReader(inputCPP));
+//		
+//		while ((tokensCPP = lexicalAnalyzer.yylex()) != null) {
+//			System.out.println("(" + tokensCPP.name + ", " + tokensCPP.value + tokensCPP.line + ")");
+//		}
 	}
-
 }
