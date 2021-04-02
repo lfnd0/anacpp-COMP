0769cb1400def9457e6acd4b4a3583d026a44040
luizfernando <luiz-fsilva37@hotmail.com>
2021-04-02 18:29:51 -0300

    Adição métodos classe TokensCPP

--- anacpp/src/br/edu/analisador/cpp/TokensCPP.java
+++ anacpp/src/br/edu/analisador/cpp/TokensCPP.java
@@ -1,5 +1,13 @@
 package br.edu.analisador.cpp;
 
 public class TokensCPP {
+	public String nome;
+	public String valor;
+	public Integer linha;
 
+	public TokensCPP(String nome, String valor, Integer linha) {
+		this.nome = nome;
+		this.valor = valor;
+		this.linha = linha;
+	}
 }
